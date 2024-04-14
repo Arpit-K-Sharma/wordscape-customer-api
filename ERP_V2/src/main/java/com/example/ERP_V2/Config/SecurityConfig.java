c69ea57f48fed6fe924643ce61beb4fd17a930a5
Sweing Nakarmi <sweing111@gmail.com>
2024-04-14 14:34:54 +0545

    Securty config added (CORS)

---
+++ ERP_V2/src/main/java/com/example/ERP_V2/Config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.ERP_V2.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig implements WebMvcConfigurer {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
+
