f6d5279e397bf20c377c5b1045716f1925309ccb
sarthakshrestha <sarthakstha24@gmail.com>
2024-06-11 13:07:43 +0545

    sarthak changes

--- ERP_V2/src/main/java/com/example/ERP_V2/ErpV2Application.java
+++ ERP_V2/src/main/java/com/example/ERP_V2/ErpV2Application.java
@@ -39,7 +39,6 @@ public class ErpV2Application implements CommandLineRunner {
 		this.costService.addInitialPaperSizes();
 		this.costService.addInitialPaperThicknesses();
 		this.adminService.createAdmin();
-
 		this.orderService.insertDummyData();
 		this.userService.insertDummyData();
 	}
