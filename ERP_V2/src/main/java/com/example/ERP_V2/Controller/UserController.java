dd5d1e76a47c78ce203dd95c8cf83117670392fa
Frozen-369 <frozenboyarp@gmail.com>
2024-06-14 13:11:48 +0545

    Added Update Deadline

--- ERP_V2/src/main/java/com/example/ERP_V2/Controller/UserController.java
+++ ERP_V2/src/main/java/com/example/ERP_V2/Controller/UserController.java
@@ -25,7 +25,7 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<List<UserDTO>> getAllUsers(
-            @RequestParam(value = "pageNumber", defaultValue = "1", required = false) Integer pageNumber,
+            @RequestParam(value = "pageNumber", defaultValue = "0", required = false) Integer pageNumber,
             @RequestParam(value = "pageSize", defaultValue = "10", required = false) Integer pageSize
     ) {
         List<UserDTO> users = userService.getAllUsers(pageNumber, pageSize);
--- ERP_V2/src/main/java/com/example/ERP_V2/Model/CostCalculation.java
+++ ERP_V2/src/main/java/com/example/ERP_V2/Model/CostCalculation.java
@@ -1,5 +1,6 @@
 package com.example.ERP_V2.Model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,32 +15,45 @@ import java.math.BigDecimal;
 public class CostCalculation {
 
     @Id
-    private String costCalculationId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-generated ID
+    private int costCalculationId;
 
     @Column(precision = 6, scale = 2)
     private BigDecimal plates;
+
     @Column(precision = 6, scale = 2)
     private BigDecimal printing;
+
     @Column(precision = 6, scale = 2)
     private BigDecimal paper;
+
     @Column(precision = 6, scale = 2)
     private BigDecimal coverPaper;
+
     @Column(precision = 6, scale = 2)
     private BigDecimal innerPaper;
+
     @Column(precision = 6, scale = 2)
     private BigDecimal otherPaper;
+
     @Column(precision = 6, scale = 2)
     private BigDecimal lamination;
+
     @Column(precision = 6, scale = 2)
     private BigDecimal binding;
+
     @Column(precision = 6, scale = 2)
     private BigDecimal finishing;
+
     @Column(precision = 6, scale = 2)
     private BigDecimal extraCharges;
+
     @Column(precision = 10, scale = 2)
     private BigDecimal subTotal;
+
     @Column(precision = 6, scale = 2)
     private BigDecimal vat;
+
     @Column(precision = 10, scale = 2)
     private BigDecimal grandTotal;
 
@@ -49,6 +63,7 @@ public class CostCalculation {
 
     @OneToOne
     @JoinColumn(name = "order_id")
+    @JsonBackReference
     private Order order;
 
 }
