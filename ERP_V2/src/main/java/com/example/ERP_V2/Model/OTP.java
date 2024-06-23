5abca57ea12d9529d708a1c14df10084c4705f7a
Siron Shakya <sironshakya10@gmail.com>
2024-06-23 08:30:09 +0545

    OTP update fix

--- ERP_V2/src/main/java/com/example/ERP_V2/Model/OTP.java
+++ ERP_V2/src/main/java/com/example/ERP_V2/Model/OTP.java
@@ -3,13 +3,19 @@ package com.example.ERP_V2.Model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
+@Document(collection = "otp")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OTP {
+
+    @Id
+    private String optId;
 
     private int otp;
 
